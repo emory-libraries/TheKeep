114fd10ff49aa77f66f7e276f2e82a6ff935794f
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2006-10-10 19:39:10 +0000

    added function to search on any of title, name (& role), image note, image format

--- app/models/content.rb
+++ app/models/content.rb
@@ -17,7 +17,38 @@ class Content < ActiveRecord::Base
     find(:all,
       :select     => 'c.id, c.record_id_type, c.other_id, c.date_created, c.date_modified, c.collection_number, c.title, c.subtitle, c.resource_type_id, c.location_id, c.abstract, c.toc, c.content_notes, c.completed_by, c.completed_date, c.complete',
       :joins      => 'AS c LEFT JOIN tech_images AS ti ON c.id = ti.content_id',
-      :conditions => '"ti"."image_note" = \'Dawson Grant\'')
+      :conditions => "\"ti\".\"image_note\" = '#{noteText}'")
+  end
+
+  def self.search(options)  
+    conditions = "1=1 "
+    joins = "AS c "
+   if (options[:title] != '')
+     conditions += "and c.title LIKE '%#{options[:title]}%'"
+   end  
+   unless (options[:name][:id] == '')
+      joins += " LEFT JOIN contents_names as cn ON c.id = cn.content_id"
+      conditions += "and cn.name_id = #{options[:name][:id]}"
+      # role is only used in tandem with a name
+      unless (options[:role][:id] == '')
+        conditions += "and cn.role_id = #{options[:role][:id]}"  
+      end 
+   end
+   if (options[:image_note] != '')
+     joins += " LEFT JOIN tech_images AS ti ON c.id = ti.content_id "
+     conditions += "and ti.image_note LIKE '%#{options[:image_note]}%'"
+   end  
+   unless (options[:image][:format] == '')
+     joins += " LEFT JOIN src_still_images AS ssi ON c.id = ssi.content_id "
+     conditions += " and ssi.form_id = #{options[:image][:format]}"
+   end
+ 
+      
+    find(:all,
+#      :select     => 'c.id, c.record_id_type, c.other_id, c.date_created, c.date_modified, c.collection_number, c.title, c.subtitle, c.resource_type_id, c.location_id, c.abstract, c.toc, c.content_notes, c.completed_by, c.completed_date',
+      :select     => '*',
+      :joins      => joins,
+      :conditions => conditions)
   end
 
   
