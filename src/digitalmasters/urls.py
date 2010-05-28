d72ee60d083cde59a3d46df4434bcd71275c642b
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2010-05-28 19:23:44 +0000

    set up django admin site  #18

--- src/digitalmasters/urls.py
+++ src/digitalmasters/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.redirect_to',  {'url': 'admin/', 'permanent' : False}),
+    (r'^admin/',  include(admin.site.urls)),
+    
     # Example:
     # (r'^digitalmasters/', include('digitalmasters.foo.urls')),
 
