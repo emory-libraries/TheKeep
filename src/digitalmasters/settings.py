91d1955c777bdd7626239e737676b5a1cbca602e
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2010-05-28 18:35:12 +0000

    skeleton django app

---
+++ src/digitalmasters/settings.py
@@ -0,0 +1,71 @@
+# Django settings for digitalmasters project.
+
+from os import path
+
+# Get the directory of this file for relative dir paths.
+# Django sets too many absolute paths.
+BASE_DIR = path.dirname(path.abspath(__file__))
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'digitalmasters.urls'
+
+TEMPLATE_DIRS = (
+    path.join(BASE_DIR, 'templates'),
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.admin',
+)
+
+EXTENSION_DIRS = (
+    path.join(BASE_DIR, '../externals/django-modules'),
+)
+
+import sys
+try:
+    sys.path.extend(EXTENSION_DIRS)
+except NameError:
+    pass # EXTENSION_DIRS not defined. This is OK; we just won't use it.
+del sys
+
+try:
+    from localsettings import *
+except ImportError:
+    import sys
+    print >>sys.stderr, 'No local settings. Trying to start, but if ' + \
+        'stuff blows up, try copying localsettings.py.sample to ' + \
+        'localsettings.py and setting appropriately for your environment.'
+    pass
+
+try:
+    # use xmlrunner if it's installed; default runner otherwise. download
+    # it from http://github.com/danielfm/unittest-xml-reporting/ to output
+    # test results in JUnit-compatible XML.
+    import xmlrunner
+    TEST_RUNNER='xmlrunner.extra.djangotestrunner.run_tests'
+    TEST_OUTPUT_DIR='test-results'
+except ImportError:
+    pass
+
