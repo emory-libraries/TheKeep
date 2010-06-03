14eaa34727f9a488f8ccf0fe6ef89d116918d32b
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2010-06-03 14:29:36 +0000

    preliminary fedora AudioObject model

--- src/digitalmasters/audio/models.py
+++ src/digitalmasters/audio/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from eulcore.fedora.models import DigitalObject, FileDatastream, RdfDatastream, XmlDatastream
+from eulcore.xmlmap.dc import DublinCore
 
-# Create your models here.
+class AudioObject(DigitalObject):
+    dc = XmlDatastream("DC", "Dublin Core", DublinCore, defaults={
+            'control_group': 'X',
+            'format': 'http://www.openarchives.org/OAI/2.0/oai_dc/',
+        })
+    rels_ext = RdfDatastream("RELS-EXT", "External Relations", defaults={
+            'control_group': 'X',
+            'format': 'info:fedora/fedora-system:FedoraRELSExt-1.0',
+        })
+    audio = FileDatastream("AUDIO", "Audio datastream", defaults={
+            'mimetype': 'audio/x-wav',
+        })
