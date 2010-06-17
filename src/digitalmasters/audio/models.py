c45cb2e4f86303a3a696327a47e66d0312c46eae
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2010-06-17 22:30:21 +0000

    minimal MODS xmlobject #26

--- src/digitalmasters/audio/models.py
+++ src/digitalmasters/audio/models.py
@@ -1,3 +1,4 @@
+from eulcore import xmlmap
 from eulcore.fedora.models import DigitalObject, FileDatastream, RdfDatastream, XmlDatastream
 from eulcore.xmlmap.dc import DublinCore
 
@@ -13,3 +14,41 @@ class AudioObject(DigitalObject):
     audio = FileDatastream("AUDIO", "Audio datastream", defaults={
             'mimetype': 'audio/x-wav',
         })
+
+class ModsDate(xmlmap.XmlObject):
+    ROOT_NAME = 'dateCreated'       # ?? could vary
+    key = xmlmap.SimpleBooleanField('@keyDate', 'yes', 'no')    # FIXME: not really a simple boolean...
+    value = xmlmap.StringField('.')     # date field?
+
+class ModsOriginInfo(xmlmap.XmlObject):
+    ROOT_NAME = 'originInfo'
+    created = xmlmap.NodeField('mods:dateCreated', ModsDate)
+
+class ModsNote(xmlmap.XmlObject):
+    ROOT_NAME = 'note'
+    label = xmlmap.StringField('@displayLabel')
+    type = xmlmap.StringField('@type')
+        # list of options:
+        # general, inscription, source of information, reference, hidden
+        # with capacity to add to the list ?
+    text = xmlmap.StringField('.')      # actual text value of the note
+
+class Mods(xmlmap.XmlObject):
+    """
+    Prototype XmlObject for MODS metadata.
+
+    Only a limited field set for now.
+    """
+
+    ROOT_NS = 'http://www.loc.gov/mods/v3'
+    ROOT_NAME = 'mods'
+    ROOT_NAMESPACES = {'mods': ROOT_NS }
+
+    XSD_SCHEMA = "http://www.loc.gov/standards/mods/mods.xsd"
+    xmlschema = xmlmap.loadSchema(XSD_SCHEMA)
+    
+    title = xmlmap.StringField("mods:titleInfo/mods:title")
+    resource_type  = xmlmap.StringField("mods:typeOfResource")  # controlled list?
+    note = xmlmap.NodeField('mods:note', ModsNote)
+    origin_info = xmlmap.NodeField('mods:originInfo', ModsOriginInfo)
+
--- src/digitalmasters/audio/tests.py
+++ src/digitalmasters/audio/tests.py
@@ -5,9 +5,10 @@ from django.core.urlresolvers import reverse
 from django.test import Client, TestCase
 
 from eulcore.django.fedora.server import Repository
+from eulcore.xmlmap  import load_xmlobject_from_string
 
 from digitalmasters.audio.forms import UploadForm, SearchForm
-from digitalmasters.audio.models import AudioObject
+from digitalmasters.audio.models import AudioObject, Mods, ModsNote, ModsOriginInfo, ModsDate
 
 class AudioTest(TestCase):
     fixtures =  ['users']
@@ -144,5 +145,53 @@ class AudioTest(TestCase):
                         (expected, response['Content-Type'], download_url))
                              
 
-        
+# tests for (prototype) MODS XmlObject
+class TestMods(TestCase):
+    FIXTURE = """<mods:mods xmlns:mods="http://www.loc.gov/mods/v3">
+  <mods:titleInfo>
+    <mods:title>A simple record</mods:title>
+  </mods:titleInfo>
+  <mods:typeOfResource>text</mods:typeOfResource>
+  <mods:note displayLabel="a general note" type="general">remember to...</mods:note>
+  <mods:originInfo>
+    <mods:dateCreated keyDate='yes'>2010-06-17</mods:dateCreated>
+  </mods:originInfo>
+</mods:mods>
+"""
+
+    def setUp(self):
+        self.mods = load_xmlobject_from_string(self.FIXTURE, Mods)
+
+    def test_init_types(self):
+        self.assert_(isinstance(self.mods, Mods))
+        self.assert_(isinstance(self.mods.note, ModsNote))
+        self.assert_(isinstance(self.mods.origin_info, ModsOriginInfo))
+        self.assert_(isinstance(self.mods.origin_info.created, ModsDate))
+
+    def test_fields(self):
+        self.assertEqual('A simple record', self.mods.title)
+        self.assertEqual('text', self.mods.resource_type)
+        self.assertEqual('a general note', self.mods.note.label)
+        self.assertEqual('general', self.mods.note.type)
+        self.assertEqual(u'remember to...', unicode(self.mods.note))
+        self.assertEqual(u'2010-06-17', unicode(self.mods.origin_info.created))
+        self.assertEqual('2010-06-17', self.mods.origin_info.created.value)
+        self.assertEqual(True, self.mods.origin_info.created.key)   # oversimplifying boolean here
+
+    def test_template_init(self):
+        mods = Mods()
+        xml = mods.serialize()
+        self.assert_('<mods:mods ' in xml)
+        self.assert_('xmlns:mods="http://www.loc.gov/mods/v3"' in xml)
+        # TODO: can't yet create from scratch because fields are nested...
+
+    def test_isvalid(self):
+        self.assertTrue(self.mods.is_valid())
+
+        invalid = """<mods:mods xmlns:mods="http://www.loc.gov/mods/v3">
+    <mods:titleInfo><mods:title invalid_attribute='oops'>An invalid record</mods:title></mods:titleInfo>
+</mods:mods>
+        """
+        invalid_mods = load_xmlobject_from_string(invalid, Mods)
+        self.assertFalse(invalid_mods.is_valid())
         
