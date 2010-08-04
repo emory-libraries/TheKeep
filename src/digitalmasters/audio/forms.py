47de7cd288b776333ceec07809c104dc8dc76339
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2010-08-04 21:44:43 +0000

    partial collection edit form #63; disable audio form components broken by changes

--- src/digitalmasters/audio/forms.py
+++ src/digitalmasters/audio/forms.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 from eulcore.django.forms import XmlObjectForm
 
-from digitalmasters.audio.models import Mods
+from digitalmasters.audio.models import Mods, CollectionMods, ModsDate, CollectionObject
 
 class UploadForm(forms.Form):
     label = forms.CharField(max_length=255, # fedora label maxes out at 255 characters
@@ -21,10 +21,53 @@ class EditForm(XmlObjectForm):
         model = Mods
         fields = [
             'title', 'resource_type', 'note',
-            'origin_info.created.date', 'origin_info.created.key_date',
+            # NOTE: disabled because created is now a list field - not yet supported
+            #'origin_info.created.date', 'origin_info.created.key_date',
             ]
 
         widgets = {
             'note' : {'text': forms.Textarea },
-            'origin_info' : { 'created' : { 'date': forms.DateInput }}
+            #'origin_info' : { 'created' : { 'date': forms.DateInput }}
             }
+
+class CollectionForm(XmlObjectForm):
+    # NOTE: this only sets choices on load time
+    # TODO: would be nice to have an ObjectChoiceField analogous to django's ModelChoiceField
+    collection = forms.ChoiceField(label="Collection",
+                    choices=[(o.pid, o.label) for o in CollectionObject.top_level()] )
+    date_created = forms.CharField(help_text="Date created, or start date for a date range.")
+    date_end = forms.CharField(help_text="End date for a date range. Leave blank if not a range.",
+                                required=False)
+    class Meta:
+        model = CollectionMods
+        fields = [
+            'source_id', 'title', 'resource_type',
+            # TODO: name
+            'restrictions_on_access.text', 'use_and_reproduction.text',
+            ]
+        widgets = {
+            'restrictions_on_access' : {'text': forms.Textarea },
+            'use_and_reproduction' : {'text': forms.Textarea }
+            }
+
+
+    def update_instance(self):
+        # override default update for additional logic
+        super(CollectionForm, self).update_instance()
+
+        # set date created - could be a single date or a date range
+        if len(self.instance.origin_info.created) == 0:
+            self.instance.origin_info.created.insert(0, ModsDate())
+        self.instance.origin_info.created[0].date = self.cleaned_data['date_created']
+        self.instance.origin_info.created[0].key_date = True
+        if 'date_end' in self.cleaned_data and self.cleaned_data['date_end']:
+            if len(self.instance.origin_info.created) == 1:
+                self.instance.origin_info.created.insert(1, ModsDate())            
+            self.instance.origin_info.created[1].date = self.cleaned_data['date_end']
+            self.instance.origin_info.created[1].point = 'end'
+            self.instance.origin_info.created[0].point = 'start'
+
+        # NOTE: parent collection - not part of the MODS
+        # will have to be set in the view?
+
+        return self.instance
