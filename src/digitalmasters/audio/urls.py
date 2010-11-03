c159e961c98fe743507fbd457613564dfc07c76a
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2010-11-03 22:15:22 +0000

    move collection-specific code into separate django app #162

--- src/digitalmasters/audio/urls.py
+++ src/digitalmasters/audio/urls.py
@@ -7,8 +7,4 @@ urlpatterns = patterns('digitalmasters.audio.views',
     url(r'^search$', 'search', name='search'),
     url(r'^(?P<pid>[^/]+)/edit$', 'edit', name='edit'),
     url(r'^(?P<pid>[^/]+)/audio$', 'download_audio', name='download-audio'),
-    url(r'^collections/new$', 'edit_collection', name='new-collection'),
-    url(r'^collections/(?P<pid>[^/]+)/edit$', 'edit_collection', name='edit-collection'),
-    url(r'^collections/search/$', 'collection_search', name='search-collections'),
-    url(r'^collections/$', 'collection_browse', name='browse-collections'),
 )
