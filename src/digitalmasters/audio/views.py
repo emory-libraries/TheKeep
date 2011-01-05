176ff21783d0d3688e2ad4af7e1b5a351522dd5d
scande3 <scande3@5739737f-271f-0410-8c82-bcd5fd3abeea>
2011-01-05 16:55:56 +0000

    #117 - removed duplicate function.

--- src/digitalmasters/audio/views.py
+++ src/digitalmasters/audio/views.py
@@ -324,33 +324,6 @@ def download_audio(request, pid):
         return HttpResponse(msg, mimetype='text/plain', status=500)
 
 @permission_required('is_staff')
-def is_compressed_audio_available(pid):
-    "Serve out the compressed audio datastream for the fedora object specified by pid."
-    repo = Repository(request=request)
-    obj = repo.get_object(pid, type=AudioObject)
-    if obj.compressed_audio._content == None:
-        return false
-    else:
-        return true
-
-@permission_required('is_staff')
-def download_compressed_audio(request, pid):
-    "Serve out the compressed audio datastream for the fedora object specified by pid."
-    repo = Repository(request=request)
-    obj = repo.get_object(pid, type=AudioObject)
-    # NOTE: this will probably need some work to be able to handle large datastreams
-    try:
-        response = HttpResponse(obj.compressed_audio.content, mimetype=obj.compressed_audio.mimetype)
-        response['Content-Disposition'] = "attachment; filename=%s.wav" % slugify(obj.label)
-        return response
-    except:
-        msg = 'There was an error contacting the digital repository. ' + \
-              'This prevented us from accessing audio data. If this ' + \
-              'problem persists, please alert the repository ' + \
-              'administrator.'
-        return HttpResponse(msg, mimetype='text/plain', status=500)
-
-@permission_required('is_staff')
 def download_compressed_audio(request, pid):
     "Serve out the compressed audio datastream for the fedora object specified by pid."
     repo = Repository(request=request)
