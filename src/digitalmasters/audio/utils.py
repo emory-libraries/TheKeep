0628660fbeab1c57eca4d5f13bf10bfac34ba016
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2011-01-19 16:59:08 +0000

    clean up checksum method

--- src/digitalmasters/audio/utils.py
+++ src/digitalmasters/audio/utils.py
@@ -1,22 +1,17 @@
-import os
 import hashlib
-import sys
 
-def md5sum(fname):
-    '''Returns an md5 hash for file fname.'''
-    #Try to open the file.
-    try:
-        f = file(fname, 'rb')
-    except:
-        return 'Failed to open file'
-    
-    #Calculate the md5
-    m = hashlib.md5()
-    while True:
-        d = f.read(8096)
-        if not d:
-            break
-        m.update(d)
-        
-    f.close()
-    return m.hexdigest()
+def md5sum(filename):
+    '''Calculate and returns an md5 checksum for the specified file.
+
+    :param filename: name of the file for which checksum should be calculated
+    :returns: hex-digest formatted MD5 checksum as a string
+    '''
+    # pythonic md5 calculation from Stack Overflow 
+    # http://stackoverflow.com/questions/1131220/get-md5-hash-of-a-files-without-open-it-in-python
+    md5 = hashlib.md5()
+    with open(filename,'rb') as f:
+        for chunk in iter(lambda: f.read(128*md5.block_size), ''):
+             md5.update(chunk)
+    return md5.hexdigest()
+
+# TODO: move into common/utils.py
