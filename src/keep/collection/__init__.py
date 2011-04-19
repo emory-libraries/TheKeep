13e7efa0198c09d346b685463d18088d7c957b2a
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2011-04-19 16:39:35 +0000

    clean up test cache handling

--- src/keep/collection/__init__.py
+++ src/keep/collection/__init__.py
@@ -2,6 +2,7 @@
 # Use test signals to swap out the default configured cache with a
 # test cache that will be used only for the duration of the test.
 
+from os import path
 from shutil import rmtree
 
 from django.conf import settings
@@ -14,17 +15,19 @@ def _swap_cache(sender, **kwargs):
     cache_backend = getattr(settings, 'CACHE_BACKEND', None)
     if cache_backend and cache_backend.startswith('file://'):
         settings.CACHE_BACKEND += '-test'
-        print "Switching to test cache : %s" % settings.CACHE_BACKEND
+        print "Switching to test cache: %s" % settings.CACHE_BACKEND
 
 
 def _restore_cache(sender, **kwargs):
     cache_backend = getattr(settings, 'CACHE_BACKEND', None)
     if cache_backend and cache_backend.startswith('file://') \
     	and cache_backend.endswith('-test'):
-        # remove the cache directory & all cache files under it
-        rmtree(cache_backend[len('file://'):])
+        # if the cache was used, remove directory & all cache files 
+        cache_dir = cache_backend[len('file://'):]
+        if path.exists(cache_dir):
+            rmtree(cache_dir)
         settings.CACHE_BACKEND = cache_backend[:len(cache_backend) - len('-test')]
-        print "Restoring cache : %s" % settings.CACHE_BACKEND
+        print "Restoring cache: %s" % settings.CACHE_BACKEND
 
 
 starting_tests.connect(_swap_cache)
