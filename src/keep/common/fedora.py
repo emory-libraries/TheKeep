d682427948dca61fe89a6ff9225e827f4a6e28ad
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2011-04-28 16:31:17 +0000

    update use object label for pid name when creating new ARKs for object pids

--- src/keep/common/fedora.py
+++ src/keep/common/fedora.py
@@ -95,8 +95,10 @@ class DigitalObject(models.DigitalObject):
             target = target.replace(self.ENCODED_PID_TOKEN, self.PID_TOKEN)
             # reverse() returns a full path - absolutize so we get scheme & server also
             target = absolutize_url(target)
+            # pid name is not required, but helpful for managing pids
+            pid_name = self.label
             # ask pidman for a new ark in the configured pidman domain
-            ark = pidman.create_ark(settings.PIDMAN_DOMAIN, target)
+            ark = pidman.create_ark(settings.PIDMAN_DOMAIN, target, name=pid_name)
             # grab the noid from the tail end of the ark
             arkbase, slash, noid = ark.rpartition('/')
             # and construct a pid in the configured pidspace
