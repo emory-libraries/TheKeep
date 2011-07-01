6524ed68420a0f1f6a9f848db55383377eae6b94
scande3 <scande3@5739737f-271f-0410-8c82-bcd5fd3abeea>
2011-07-01 22:03:58 +0000

    #292 - added a permission to the models and two test users (one that uses and one that does not use the permission).

--- keep/arrangement/models.py
+++ keep/arrangement/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+class Permissions(models.Model):
+    class Meta:
+        permissions = (
+            ("marbl_allowed", "Access to MARBL collections is allowed."),
+        )
 
 # Create your models here.
--- keep/arrangement/tests.py
+++ keep/arrangement/tests.py
@@ -7,17 +7,17 @@ Replace these with more appropriate tests for your application.
 
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from django.contrib.auth.models import User
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class PermissionsCheckTest(TestCase):
+    fixtures =  ['users']
 
->>> 1 + 1 == 2
-True
-"""}
+    def test_permission_exists(self):
+        #Test for permission on a sample fixture user
+        marbl_user = User.objects.get(username__exact='marbl')
+        self.assertTrue(marbl_user.has_perm('arrangement.marbl_allowed'))
+
+        #Test for permission not existing on a sample fixture user.
+        non_marbl_user = User.objects.get(username__exact='nonmarbl')
+        self.assertFalse(non_marbl_user.has_perm('arrangement.marbl_allowed'))
 
