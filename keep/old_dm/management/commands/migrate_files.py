48c8864f4f6d0efa9db236b1c1d183892535c7b8
branker <branker@5739737f-271f-0410-8c82-bcd5fd3abeea>
2011-07-07 21:10:19 +0000

    log expected path for file migration

--- keep/old_dm/management/commands/migrate_files.py
+++ keep/old_dm/management/commands/migrate_files.py
@@ -1,7 +1,11 @@
 from collections import defaultdict
 import logging
+import os
 import sys
+
+from django.conf import settings
 from django.core.management.base import BaseCommand
+
 from keep.audio.models import AudioObject
 from keep.common.fedora import Repository
 
@@ -15,13 +19,21 @@ class Command(BaseCommand):
             stats['audio'] += 1
             mods = obj.mods.content
             old_id = mods.dm1_other_id or mods.dm1_id
-            if old_id:
-                stats['dm1'] += 1
-                logger.info('Found %s=%s %s' % (obj.pid, old_id, mods.title))
-            else:
+            if not old_id:
                 logger.debug('%s: no DM1 id. skipping.' % (obj.pid,))
+                continue
 
-        logger.info('Total DM1 objects: %(dm1)d (of %(audio)d audio objects)' % stats)
+            stats['dm1'] += 1
+            logger.info('Found %s=%s %s' % (obj.pid, old_id, mods.title))
+
+            obj_path = obj.old_dm_media_path()
+            if not obj_path:
+                logger.error("%s: couldn't predict path. skipping." % (obj.pid,))
+                continue
+            expect_path = os.path.join(settings.MIGRATION_AUDIO_ROOT, obj_path)
+            logger.info('%s should be at %s' % (obj.pid, expect_path))
+
+        logger.debug('Total DM1 objects: %(dm1)d (of %(audio)d audio objects)' % stats)
 
     def audio_objects(self):
         repo = Repository()
