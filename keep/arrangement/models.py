7203482bd3929eeddbbdbb4f5d75087686d713e9
athom09 <athom09@5739737f-271f-0410-8c82-bcd5fd3abeea>
2011-07-08 18:49:50 +0000

    Rushdie 317 - Added ProcessingBatch in arrangement.models

--- keep/arrangement/models.py
+++ keep/arrangement/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from rdflib import RDF
+from keep.collection.models import SimpleCollection
+from keep.common.rdfns import REPO
+
 class Permissions(models.Model):
     class Meta:
         permissions = (
             ("marbl_allowed", "Access to MARBL collections is allowed."),
         )
 
-# Create your models here.
+
+class ProcessingBatch(SimpleCollection):
+    def __init__(self, *args, **kwargs):
+        super(ProcessingBatch, self).__init__(*args, **kwargs)
+
+        #set RDF.type in rels_ext
+        self.rels_ext.content.add((self.uri, RDF.type, REPO.ProcessingBatch))
---
+++ keep/common/rdfns.py
@@ -0,0 +1,5 @@
+from rdflib.namespace import Namespace
+from rdflib import URIRef
+
+REPO = Namespace(URIRef('http://pid.emory.edu/ns/2011/repo-management/#'))
+  
