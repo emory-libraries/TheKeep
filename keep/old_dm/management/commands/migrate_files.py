c296e8b153031aa0db1eb6f532480bdc0e831087
branker <branker@5739737f-271f-0410-8c82-bcd5fd3abeea>
2011-07-11 15:11:05 +0000

    check for wav, m4a, md5, and jhove for each record. #336

--- keep/old_dm/management/commands/migrate_files.py
+++ keep/old_dm/management/commands/migrate_files.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 import logging
 import os
 import sys
@@ -10,6 +10,9 @@ from keep.audio.models import AudioObject
 from keep.common.fedora import Repository
 
 logger = logging.getLogger(__name__)
+
+AudioFile = namedtuple('AudioFile', 
+        ('wav', 'm4a', 'md5', 'jhove'))
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -25,13 +28,11 @@ class Command(BaseCommand):
 
             stats['dm1'] += 1
             logger.info('Found %s=%s %s' % (obj.pid, old_id, mods.title))
-
-            obj_path = obj.old_dm_media_path()
-            if not obj_path:
+            paths = self.look_for_files(obj)
+            if not paths:
                 logger.error("%s: couldn't predict path. skipping." % (obj.pid,))
                 continue
-            expect_path = os.path.join(settings.MIGRATION_AUDIO_ROOT, obj_path)
-            logger.info('%s should be at %s' % (obj.pid, expect_path))
+            logger.info('%s paths: %s' % (obj.pid, repr(paths)))
 
         logger.debug('Total DM1 objects: %(dm1)d (of %(audio)d audio objects)' % stats)
 
@@ -39,3 +40,21 @@ class Command(BaseCommand):
         repo = Repository()
         cmodel = AudioObject.AUDIO_CONTENT_MODEL
         return repo.get_objects_with_cmodel(cmodel, type=AudioObject)
+
+    def look_for_files(self, obj):
+        access_path = obj.old_dm_media_path()
+        if not access_path:
+            return
+        basename, ext = os.path.splitext(access_path)
+
+        return AudioFile(*(self.dm_path(basename, ext)
+                           for ext in ('wav', 'm4a', 'wav.md5', 'wav.jhove')))
+
+    def dm_path(self, basename, ext):
+        rel_path = '%s.%s' % (basename, ext)
+        abs_path = os.path.join(settings.MIGRATION_AUDIO_ROOT, rel_path)
+        if os.path.exists(abs_path):
+            logging.debug('found path: ' + abs_path)
+            return abs_path
+        else:
+            logging.debug('missing path: ' + abs_path)
