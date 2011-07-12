1b0353f108295a8a5ddfaf7fc31ee561c9f19ecb
branker <branker@5739737f-271f-0410-8c82-bcd5fd3abeea>
2011-07-12 19:29:58 +0000

    csv output option from file migration

--- keep/old_dm/management/commands/migrate_files.py
+++ keep/old_dm/management/commands/migrate_files.py
@@ -1,5 +1,8 @@
 from collections import defaultdict, namedtuple
+from contextlib import contextmanager
+import csv
 import logging
+from optparse import make_option
 import os
 import sys
 
@@ -15,26 +18,53 @@ AudioFile = namedtuple('AudioFile',
         ('wav', 'm4a', 'md5', 'jhove'))
 
 class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--csvoutput', '-c',
+            help='''Output CSV data to the specified filename'''),
+        )
+
     def handle(self, *args, **options):
         stats = defaultdict(int)
 
-        for obj in self.audio_objects():
-            stats['audio'] += 1
-            mods = obj.mods.content
-            old_id = mods.dm1_other_id or mods.dm1_id
-            if not old_id:
-                logger.debug('%s: no DM1 id. skipping.' % (obj.pid,))
-                continue
+        with self.open_csv(options) as csvfile:
+            if csvfile:
+                FIELDS = ('pid', 'dm1_id', 'dm1_other_id',
+                          'wav', 'm4a', 'md5', 'jhove')
+                csvfile.writerow(FIELDS)
 
-            stats['dm1'] += 1
-            logger.info('Found %s=%s %s' % (obj.pid, old_id, mods.title))
-            paths = self.look_for_files(obj)
-            if not paths:
-                logger.error("%s: couldn't predict path. skipping." % (obj.pid,))
-                continue
-            logger.info('%s paths: %s' % (obj.pid, repr(paths)))
+            for obj in self.audio_objects():
+                stats['audio'] += 1
+                mods = obj.mods.content
+                old_id = mods.dm1_other_id or mods.dm1_id
+                if not old_id:
+                    logger.debug('%s: no DM1 id. skipping.' % (obj.pid,))
+                    continue
+
+                stats['dm1'] += 1
+                logger.info('Found %s=%s %s' % (obj.pid, old_id, mods.title))
+                paths = self.look_for_files(obj)
+                if not paths:
+                    logger.error("%s: couldn't predict path. skipping." % (obj.pid,))
+                    continue
+                logger.info('%s paths: %s' % (obj.pid, repr(paths)))
+
+                if csvfile:
+                    row_data = [ obj.pid, obj.mods.content.dm1_id,
+                                 obj.mods.content.dm1_other_id ] + \
+                                 list(paths)
+                    csvfile.writerow(row_data)
+
 
         logger.debug('Total DM1 objects: %(dm1)d (of %(audio)d audio objects)' % stats)
+
+    @contextmanager
+    def open_csv(self, options):
+        if options['csvoutput']:
+            with open(options['csvoutput'], 'wb') as f:
+                csvfile = csv.writer(f)
+                yield csvfile
+        else:
+            yield None
 
     def audio_objects(self):
         repo = Repository()
