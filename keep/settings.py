201d26b00b9e048acf77c1cc11627af1e8877529
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2012-04-19 22:22:04 +0000

    add south to deps/installed apps, document migrations for emory_ldap and djcelery

--- keep/settings.py
+++ keep/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'south',
     'eulexistdb',
     'eulfedora',
     'eulcommon.searchutil',
