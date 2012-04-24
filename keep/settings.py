bb0103716d9b1bd8c4ff7970f53dd9fa19850b21
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2012-04-24 19:58:32 +0000

    update settings and site urls to take advantage of staticfiles [#28316963]

--- keep/settings.py
+++ keep/settings.py
@@ -15,10 +15,37 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'sitemedia')
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = '/static'
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, '..', 'sitemedia'),
+    os.path.join(os.environ['VIRTUAL_ENV'], 'themes', 'genlib'),
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -76,6 +103,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.staticfiles',
     'django.contrib.humanize',
     'south',
     'eulexistdb',
--- keep/urls.py
+++ keep/urls.py
@@ -31,29 +31,4 @@ urlpatterns = patterns('',
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
 )
-
-
-# DISABLE THIS IN PRODUCTION
-if settings.DEV_ENV:
-    import os
-    # if there's not a genlib_media dir/link in the media directory, then
-    # look for it in the virtualenv themes.
-    if not os.path.exists(os.path.join(settings.MEDIA_ROOT, 'genlib_media')) and \
-            'VIRTUAL_ENV' in os.environ:
-        genlib_media_root = os.path.join(os.environ['VIRTUAL_ENV'],
-                                         'themes', 'genlib', 'genlib_media')
-        urlpatterns += patterns('',
-            (r'^static/genlib_media/(?P<path>.*)$', 'django.views.static.serve', {
-                'document_root': genlib_media_root,
-                }),
-        )
-
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT,
-            }),
-    )
-
