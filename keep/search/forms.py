69963b953856aa111c1e89d86efec77e65a84cb1
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2012-04-24 19:55:22 +0000

    solr search term validation for combined search [#26883729]

--- keep/search/forms.py
+++ keep/search/forms.py
@@ -1,4 +1,33 @@
+import re
 from django import forms
+from eulcommon.searchutil import search_terms
+
+
+class SolrSearchField(forms.CharField):
+    '''Extension of :class:`django.forms.CharField` with validation
+    and conversion for use with Solr searching via :mod:`sunburnt`.
+
+    Returns a list of keyword terms tokenized into words and phrases
+    by :meth:`eulcommon.searchutil.search_terms`.  Validates that none
+    of the tokenized search terms starts with a wildcard * or ?, since
+    Solr does not handle wildcards at the beginning of search terms.
+    '''
+
+    invalid = 'Search terms may not begin with wildcards * or ?'
+    
+    def to_python(self, value):
+        if not value:
+            return []
+        else:
+            return search_terms(value)
+        
+    def validate(self, value):
+        super(forms.CharField, self).validate(' '.join(value))
+        for v in value:
+            if v.startswith('*') or v.startswith('?'):
+                raise forms.ValidationError(self.invalid)
+
 
 class KeywordSearch(forms.Form):
-    keyword = forms.CharField(required=True)
+    keyword = SolrSearchField(required=False)
+        
--- keep/search/tests.py
+++ keep/search/tests.py
@@ -1,16 +1,38 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from django import forms
 from django.test import TestCase
 
+from keep.search.forms import SolrSearchField
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+class SolrSearchFieldTest(TestCase):
+
+    def setUp(self):
+        self.field = SolrSearchField(required=True)
+
+    def test_to_python(self):
+        self.assert_(isinstance(self.field.to_python(''), list),
+                     'to python should return a list, even for an empty value')
+        self.assert_(isinstance(self.field.to_python('one two three'), list),
+                     'to python should return a list')
+
+    def test_validate(self):
+        # inherited validation - required field error
+        self.assertRaises(forms.ValidationError,
+                          self.field.validate, [])
+
+        # when not required: empty should be valid
+        not_req = SolrSearchField(required=False)
+        not_req.validate([])
+
+        # no validation error should be raised 
+        self.field.validate(['one', 'two'])
+        self.field.validate(['one', 'two', '"three four"'])
+        self.field.validate(['one', 'two', '"three *four"'])
+
+        # validation errors should be raised
+        self.assertRaises(forms.ValidationError,
+                          self.field.validate, ['*foo'])
+        self.assertRaises(forms.ValidationError,
+                          self.field.validate, ['foo', '*bar'])
+        self.assertRaises(forms.ValidationError,
+                          self.field.validate, ['"foo bar"', '*baz'])
