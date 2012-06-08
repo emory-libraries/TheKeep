f2a6c0197f0bbf760d138fb45a7cea073428d319
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2012-06-08 21:52:35 +0000

    update import verdict script to set series and subseries details in records using current nested mods series structure [#28385583]

--- keep/arrangement/management/commands/import_verdicts.py
+++ keep/arrangement/management/commands/import_verdicts.py
@@ -7,6 +7,7 @@ from eulfedora.util import RequestFailed
 
 from keep.arrangement.models import ArrangementObject
 from keep.common.models import rights_access_terms_dict
+from keep.common.eadmap import Series
 
 class Command(BaseCommand):
     help = 'Import verdicts from a CSV file to arrangement objects in the repository'
@@ -16,7 +17,7 @@ class Command(BaseCommand):
                     help='CSV file with verdict information (REQUIRED)'),
         make_option('-n', '--noact', action='store_true', default=False,
                     help='''Test run: report what would be done, but do not modify
-                    anything in the repository'''),
+                    anythin in the repository'''),
         )
 
     # expected columns in the csv file
@@ -35,11 +36,17 @@ class Command(BaseCommand):
     # default django verbosity levels: 0 = none, 1 = normal, 2 = all
     v_normal = 1
 
+    rushdie_ead_ark = 'http://pid.emory.edu/ark:/25593/8zv36'
+    rushdie_eadid = 'rushdie1000'
+    rushdie_ead_baseurl = 'https://findingaids.library.emory.edu/documents/rushdie1000/'
+
     def handle(self, csvfile=None, verbosity=1, noact=False, *args, **options):
         if csvfile is None:
             raise CommandError('CSV filename is required')
-        verbosity = int(verbosity)  # ensure we compare int to int
+        self.verbosity = int(verbosity)  # ensure we compare int to int
 
+        # load findingaid series/subseries info
+        self.load_series_subseries()
 
         csvreader = csv.DictReader(open(csvfile, 'rb'),
                                 fieldnames=self.csv_fields)
@@ -48,11 +55,12 @@ class Command(BaseCommand):
 
         stats = defaultdict(int)
         verdict_stats = defaultdict(int)
+        series_stats = defaultdict(int)
         for row in csvreader:
             stats['rows'] += 1
             try:
                 obj = ArrangementObject.by_arrangement_id(row['id'])
-                if verbosity > self.v_normal:
+                if self.verbosity > self.v_normal:
                     print 'Found %s for arrangement id %s' % \
                           (obj.pid, row['id'])
                     
@@ -60,81 +68,224 @@ class Command(BaseCommand):
 
                 # set rights status based on verdict in csv
 
-                # TODO: what are we doing with missing/empty verdict ? 
-                
+                # TODO: what are we doing with missing/empty verdict ?
+                # skipping for now
                 if not row['verdict']:
-                    if verbosity >= self.v_normal:
+                    if self.verbosity >= self.v_normal:
                         print 'No verdict set for %(id)s' % row
                     continue
                 
-                verdict = row['verdict'].strip()
-                if verdict not in self.csv_verdict:
-                    if verbosity >= self.v_normal:
-                        print 'Verdict "%(verdict)s" not recognized (%(id)s)' % row
-                    continue
-                else:
-                    # set code & text based on verdict in CSV
+                # set access code, 
+                verdict_assigned = self.set_access_status(obj,
+                                                          row['verdict'], row)
+                if verdict_assigned:
+                    # update tally for this verdict if successful
+                    verdict_stats[verdict_assigned] += 1
 
-                    # TODO: for email, we will need to handle numeric code
+                # set series/subseries information
+                series = self.set_series(obj, row)
+                if series:
+                    series_stats[series] += 1
+
+                # if not noact mode, save object
+                if not noact:
+                    try:
+                        # only save if changed, so we can keep track of
+                        # how many updates are made
+                        if obj.rights.isModified() or obj.mods.isModified():  
+                            updated = obj.save('import verdict & series/subseries')
+                            stats['updated'] += 1
+                            
+                    except RequestFailed:
+                        print 'Error saving %s' % obj.pid
+                        stats['save_error'] += 1
                     
-                    access_code = self.csv_verdict[verdict]
-
-                    # report previous status in case we're changing a previously
-                    # assigned verdict
-                    if verbosity >= self.v_normal and obj.rights.exists \
-                       and obj.rights.content.access_status and \
-                       obj.rights.content.access_status.code != access_code:
-                        
-                        # 5300c imported as 10/undetermined; only report 10
-                        # in full verbose mode
-                        if obj.rights.content.access_status.code != '10' or \
-                           verbosity > self.v_normal:
-                            print 'Previous access code for %s was %s; changing to %s' % \
-                                  (row['id'], obj.rights.content.access_status.code,
-                                   access_code)
-
-                    # set rights code & text based on verdict
-                    obj.rights.content.create_access_status()
-                    obj.rights.content.access_status.code = access_code 
-                    obj.rights.content.access_status.text = rights_access_terms_dict[access_code].text
-
-                    # if not noact mode, save object
-                    if not noact:
-                        try:
-                            # only save if changed, so we can keep track of
-                            # how many updates are made
-                            if obj.rights.isModified():  
-                                updated = obj.save('import verdict')
-                                stats['updated'] += 1
-                                
-                        except RequestFailed:
-                            print 'Error saving %s' % obj.pid
-                            stats['save_error'] += 1
-                    
-                    # update tally for this verdict
-                    verdict_stats[verdict] += 1
 
             except ObjectDoesNotExist as err:
-                stats['not_found'] += 1
-                if verbosity >= self.v_normal:
+                stats['not_found'] +=1
+                if self.verbosity >= self.v_normal:
                     print 'Error: %s' % err
             except MultipleObjectsReturned as err:
                 stats['too_many'] += 1   
-                if verbosity >= self.v_normal:
+                if self.verbosity >= self.v_normal:
                     print 'Error: %s' % err
 
 
         # summary
-        if verbosity >= self.v_normal:
+        if self.verbosity >= self.v_normal:
             print '''\nProcessed %(rows)d rows and found %(found)d corresponding record(s)
 %(not_found)d record(s) not found, %(too_many)d with multiple matches''' % stats
             print 'Verdicts imported:\n    ' + \
-                  '; '.join('%d %s' % (n, v) for v, n in verdict_stats.iteritems())
+                  '; '.join('%d %s' % (n, v)
+                            for v, n in verdict_stats.iteritems())
+            print 'Series and subseries assigned:\n  '+ \
+                  '\n  '.join('%s : %d' % (v, n)
+                            for v, n in series_stats.iteritems())
             if not noact:
                 print 'Updated %(updated)d record(s); error saving %(save_error)d records' \
                       % stats
                     
             
 
+    def set_access_status(self, obj, verdict, data):
+        '''
+        Set the access status code on an object based on a text
+        verdict string from a CSV file
+
+        :returns: False for an unrecognized verdict; normalized
+            verdict string for successful update.
+        '''
+        
+        verdict = verdict.strip()
+        if verdict not in self.csv_verdict:
+            if self.verbosity >= self.v_normal:
+                print 'Verdict "%(verdict)s" not recognized (%(id)s)' % data
+                
+            # stop processing
+            return False
+
+    
+        # set code & text based on verdict in CSV
+
+        # TODO: for email, we will need to handle numeric code
+        access_code = self.csv_verdict[verdict]
+
+        # report previous status in case we're changing a previously
+        # assigned verdict
+        if self.verbosity >= self.v_normal and obj.rights.exists \
+               and obj.rights.content.access_status and \
+            obj.rights.content.access_status.code != access_code:
+        
+            # 5300c imported as 10/undetermined; only report 10
+            # in full verbose mode
+            if obj.rights.content.access_status.code != '10' or \
+                   self.verbosity > self.v_normal:
+                print 'Previous access code for %s was %s; changing to %s' % \
+                      (data['id'], obj.rights.content.access_status.code,
+                       access_code)
+
+        # set rights code & text based on verdict
+        obj.rights.content.create_access_status()
+        obj.rights.content.access_status.code = access_code 
+        obj.rights.content.access_status.text = rights_access_terms_dict[access_code].text
+        
+        return verdict
 
         
+    def load_series_subseries(self):
+        # load series/subseries deails to map csv series names
+
+        # query the rushdie findingaid for series/subseries info
+        series = Series.objects.filter(eadid=self.rushdie_eadid)\
+                 	.only('id', 'did__unittitle', 'subseries', 'eadid')
+
+        # create dictionaries to look up values
+        by_id = {}  # full details by id
+        by_name = {} # name -> id to map from csv file
+        for s in series:
+            by_id[s.id] = {'title': s.title, 'short_id': s.short_id}
+            by_name[s.title] = s.id
+            #print 'title = %s id = %s' % (s.title, s.id)
+            for sub in s.subseries:
+                #print 'title = %s id = %s' % (sub.title, sub.id)
+                by_id[sub.id] = {'title': sub.title, 'short_id': sub.short_id,
+                                'series': s.id}
+                by_name[sub.title] = sub.id
+
+        self.series_by_id = by_id
+        self.series_by_name = by_name
+
+
+    def set_series(self, obj, info):
+        '''
+        Update series and subseries for an object based on a text
+        series names from CSV file data.
+
+        :returns: False if series is not recognized; returns a
+            normalized version with the series and subseries applied
+            for a successful update.
+        '''
+
+        # convert text series/subseries rom CSV to mods
+        series = info['series'].strip()
+        # TODO: check that this conversion is correct 
+        if series == 'Writings':
+            series = 'Writings by Rushdie'
+        subseries = info['subseries'].strip()
+
+        # check if either series or subseries can't be mapped
+        # -- if set but not recognized , bail out
+        if series not in self.series_by_name:
+            if self.verbosity >= self.v_normal:
+                print 'Series "%(series)s" not recognized (%(id)s)' \
+                      % info
+                return False
+            
+        if subseries and subseries not in self.series_by_name:
+            if self.verbosity >= self.v_normal:
+                print 'Subseries "%(subseries)s" not recognized (%(id)s)' \
+                      % info
+            return False
+
+        # set series details in mods
+                    
+        # NOTE: exact structure is undocumented and probably not
+        # exactly what we really want;
+        # seems to be subseries at outer level, with series nested under,
+        # even in cases with no subseries (?)
+
+        # FIXME: is this order correct ? 
+    
+        # store previous series value (if any), to report if there is
+        # a change
+        prev_series = None
+        if obj.mods.content.series and obj.mods.content.series.title:
+            if obj.mods.content.series.series and \
+                   obj.mods.content.series.series.title:
+                prev_series = '%s > %s' % \
+                              (obj.mods.content.series.series.title,
+                               obj.mods.content.series.title)
+            else:
+                prev_series = obj.mods.content.series.title
+
+        obj.mods.content.create_series()
+        obj.mods.content.series.create_series()
+        self.set_series_info(obj.mods.content.series.series,
+                                           series)
+
+        # if there is a subseries, set it
+        if subseries:
+            self.set_series_info(obj.mods.content.series, subseries)
+
+            new_series = '%s > %s' % (series, subseries)
+
+        else:
+            new_series = series
+
+        # if there was a previous value different from the new one,
+        # report both new and old series/subseries
+        if prev_series is not None and prev_series != new_series \
+               and self.verbosity >= self.v_normal:
+            print 'Previous series for %s was %s; now %s' % \
+                  (obj.pid, prev_series, new_series)
+
+
+    def set_series_info(self, mods_series, series):
+        '''
+        Given a mods series object and a series name, look up series
+        information and set series details in the mods object.
+        '''
+        series_id = self.series_by_name[series]
+        series_info = self.series_by_id[series_id]        
+        mods_series.title = series_info['title']
+        mods_series.full_id = series_id
+        mods_series.short_id = series_info['short_id']
+        mods_series.base_ark = self.rushdie_ead_ark
+        # construct url to this series or subseries
+        url_parts = [self.rushdie_ead_baseurl]
+        # if this is subseries, parent series id goes first
+        if 'series' in series_info and series_info['series']: 
+            url_parts.append(self.series_by_id[series_info['series']]['short_id'])
+        url_parts.append(series_info['short_id'])
+        mods_series.uri = '/'.join(url_parts)
+
