bd4bc3544a326c594afcc0d2cc32f0d8ad6e238f
athom09 <athom09@5739737f-271f-0410-8c82-bcd5fd3abeea>
2012-09-04 21:35:42 +0000

    [#33936729] modified search views to handel collection searches

--- keep/search/views.py
+++ keep/search/views.py
@@ -113,6 +113,9 @@ def keyword_search(request):
             else:
                 solr_field = searchform.allowed_fields[field]
                 search_val = val
+                # special case for searching for collection source id
+                if field == 'coll' and search_val and search_val.isdigit():
+                    solr_field = 'collection_source_id'
                 # add wildcard to end of search dates
                 # (indexed by YYYY-MM-DD; allow match on YYYY or YYYY-MM)
                 if field == 'created':
@@ -222,6 +225,7 @@ def keyword_search(request):
                 if show_facets:
                     facets[display_name] = show_facets
 
+        del facets['coll'] # delete coll facet because it is redundant with collection facet
         ctx.update({
             'page': results,
             'show_pages': show_pages,
@@ -316,9 +320,10 @@ def keyword_search_suggest(request):
                 sort = 'count'
                 category = 'Collections'
                 result_fmt = '"%s" '
-                # if collection number facet by collection_source_id
-#                if parsed_terms[1].isdigit():
-#                    facet_field = 'collection_source_id'
+
+                #if the term is numbric facet by source_id
+                if prefix and prefix.isdigit():
+                    facet_field = 'collection_source_id_facet'
 
             solr = solr_interface()
             facetq = solr.query().paginate(rows=0)
