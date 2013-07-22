9c38993ccf02fa8538b13ab80b36d565881909c5
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2013-07-22 22:11:40 +0000

    minor cleanup

--- keep/common/context_processors.py
+++ keep/common/context_processors.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 
+
 def common_settings(request):
     '''Template context processor: add selected setting to context
     so it can be used on any page .'''
 
-    context_extras = {}
-
-    context_extras['ENABLE_BETA_WARNING'] =  getattr(settings, 'ENABLE_BETA_WARNING', False)
+    context_extras = {
+        'ENABLE_BETA_WARNING': getattr(settings, 'ENABLE_BETA_WARNING', False),
+    }
 
     return context_extras
