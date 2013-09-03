62e6abfc4a142946c3beb4d7e558f2d8467e13aa
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2013-09-03 17:26:04 +0000

    disable media url (no longer in use)

--- keep/settings.py
+++ keep/settings.py
@@ -13,7 +13,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'sitemedia')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/static'
+# MEDIA_URL = '/static'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
