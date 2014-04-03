1d1bc7e41cb93076185baae8bed0e60fb6801622
keep <keep@5739737f-271f-0410-8c82-bcd5fd3abeea>
2014-04-03 16:00:46 +0000

    rename admin search in context processor / all templates

--- keep/repoadmin/context_processors.py
+++ keep/repoadmin/context_processors.py
@@ -4,5 +4,5 @@ def search(request):
     '''Template context processor: add the keyword search form
     (:class:`~keep.repoadmin.forms.KeywordSearch`) to context
     so it can be used on any page (e.g., in the site sidebar).'''
-    return {'keyword_search': KeywordSearch()}
+    return {'admin_search': KeywordSearch()}
 
