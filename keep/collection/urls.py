7e4229d3af3cca165406c5fb21b297ec78dfa7c8
rsutton <rsutton@5739737f-271f-0410-8c82-bcd5fd3abeea>
2014-04-22 17:21:33 +0000

    tweak archive url order so it doesn't match named collection url

--- keep/collection/urls.py
+++ keep/collection/urls.py
@@ -3,12 +3,13 @@ from keep.collection import views
 
 urlpatterns = patterns('',
     url(r'^$', views.list_archives, name='list-archives'),
-    url(r'^(?P<archive>[a-z]+)/$', views.browse_archive, name='browse-archive'),
     url(r'^new/$', views.edit, name='new'),
     url(r'^search/$', views.search, name='search'),
     url(r'^suggest/$', views.collection_suggest, name='suggest'),
     url(r'^simple/$', views.simple_browse, name='simple_browse'),
     url(r'^simple/(?P<pid>[^/]+)/edit/$', views.simple_edit, name='simple_edit'),
+    # NOTE: archive must come after so we don't match named views as archive aliases
+    url(r'^(?P<archive>[a-z]+)/$', views.browse_archive, name='browse-archive'),
     url(r'^(?P<pid>[^/]+)/$', views.view, name='view'),
     url(r'^(?P<pid>[^/]+)/edit/$', views.edit, name='edit'),
     url(r'^(?P<pid>[^/]+)/history/$', views.history, name='history'),
