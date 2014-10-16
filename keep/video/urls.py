46c05d7a5d2d96bd6f68196090b1671fa5c90bc5
Alex Thomas <athom09@emory.edu>
2014-10-16 12:57:10 -0400

    [Finishes #73635976] Exposed auto-populated data and datastreams in view view

--- keep/video/urls.py
+++ keep/video/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import *
 from keep.video import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<pid>[^/]+)/$', views.view, name='view')
+    url(r'^(?P<pid>[^/]+)/$', views.view, name='view'),
+    url(r'^(?P<pid>[^/]+)/history/$', views.history, name='history'),
+    url(r'^ds/(?P<pid>[^/]+)/(?P<dsid>[a-zA-Z-0-9]+)/$',
+        views.view_datastream, name='raw-ds'),
+    url(r'^(?P<pid>[^/]+)/AUDIT/$', views.view_audit_trail, name='audit-trail')
 )
--- keep/video/views.py
+++ keep/video/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import Http404
+
+from keep.common.fedora import Repository, TypeInferringRepository
+from keep.video.models import Video
+from keep.common.fedora import history_view
+from eulfedora.views import raw_datastream, raw_audit_trail
+
+
 
 def view(request, pid):
     '''View a single :class:`~keep.video.models.Video`.
@@ -17,5 +25,38 @@ def view(request, pid):
     #
     # return TemplateResponse(request, 'audio/view.html', {'resource': obj})
 
-    return HttpResponse("You have reached this page with param %s" % pid)
+    repo = Repository(request=request)
+    obj = repo.get_object(pid=pid, type=Video)
 
+    try:
+        if not obj.has_requisite_content_models:
+            raise Http404
+    except:
+        raise Http404
+
+    return render(request, 'video/view.html', {"obj": obj})
+
+
+def history(request, pid):
+    'Display human-readable audit trail information.'
+    return history_view(request, pid, type=Video, template_name='video/history.html')
+
+def view_audit_trail(request, pid):
+    'Access XML audit trail'
+    # initialize local repo with logged-in user credentials & call eulfedora view
+    # type shouldn't matter for audit trail
+    return raw_audit_trail(request, pid, repo=Repository(request=request))
+
+
+def view_datastream(request, pid, dsid):
+    'Access raw object datastreams'
+    # initialize local repo with logged-in user credentials & call generic view
+    # use type-inferring repo to pick up rushdie file or generic arrangement
+    response = raw_datastream(request, pid, dsid,
+                          repo=TypeInferringRepository(request=request))
+
+    # work-around for email MIME data : display as plain text so it
+    # can be viewed in the browser
+    if response.get('Content-Type', None) == 'message/rfc822':
+        response['Content-Type'] = 'text/plain'
+    return response
