fd841f5545c78a40103566dbaf25053403a4f18d
Alex Thomas <athom09@emory.edu>
2015-08-06 12:35:52 -0400

    Clean up some files

--- lfi_mount.sh
+++ lfi_mount.sh
@@ -1,1 +1,1 @@
-./sshfsmount.sh -h wlibdev002.library.emory.edu -m /home/athom09/inbound -p /usr/local/webapps/fedora34/data/inbound -r athom09 -u tomcat
+./sshfsmount.sh -h libfeddev1.library.emory.edu -m /home/athom09/inbound -p /usr/local/fedora38/data/inbound -r athom09 -u fedora
