aeba29bc327c79d1799b03c4431b3a6ca4973214
Alex Thomas <athom09@emory.edu>
2015-08-19 14:00:24 -0400

    fixed collection text

--- keep/collection/tests.py
+++ keep/collection/tests.py
@@ -577,8 +577,8 @@ class CollectionViewsTest(KeepTestCase):
         self.assertEqual(COLLECTION_DATA['title'], obj.mods.content.title,
             "MODS content updated in existing object from form data")
         # confirm that current site user appears in fedora audit trail
-        xml, uri = obj.api.getObjectXML(obj.pid)
-        self.assert_('<audit:responsibility>%s</audit:responsibility>' % ADMIN_CREDENTIALS['username'] in xml,
+        xml = obj.api.getObjectXML(obj.pid)
+        self.assert_('<audit:responsibility>%s</audit:responsibility>' % ADMIN_CREDENTIALS['username'] in xml.content,
             'user logged into site is also username in fedora audit:trail')
 
         #test audit trail
