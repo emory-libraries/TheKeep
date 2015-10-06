ab4f8e3e757313fe7643fafb350fc206fe26131c
rlskoeser <rebecca.s.koeser@emory.edu>
2015-10-06 12:50:20 -0400

    Remove outdated and unused test runner

--- keep/testutil.py
+++ keep/testutil.py
@@ -9,18 +9,13 @@ cache so that tests don't stomp on the cache used by other commands. After
 testing it swaps the regular cache back into place.
 """
 
-from contextlib import nested
-import os
-import unittest
-from shutil import rmtree
 import sunburnt
 from mock import Mock
 
 from django.conf import settings
-from django.test.simple import DjangoTestSuiteRunner
+# from django.test.simple import DjangoTestSuiteRunner
 
 from eulexistdb import testutil as existdb_testutil
-from eulfedora import testutil as fedora_testutil
 from eulfedora.server import Repository
 
 
@@ -58,83 +53,3 @@ class KeepTestCase(existdb_testutil.TestCase):
         else:
             del settings.SOLR_SERVER_URL
 
-
-
-
-class CacheTestWrapper(object):
-    # TODO: could this go in eulcommon?
-    def __init__(self):
-        self.stored_cache_backend = None
-
-    def __enter__(self):
-        self.swap_cache()
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.restore_cache()
-
-    def swap_cache(self):
-        # use a test cache when running tests to avoid conflicts with runserver cache
-        cache_backend = getattr(settings, 'CACHE_BACKEND', None)
-        if cache_backend and cache_backend.startswith('file://'):
-            self.stored_cache_backend = settings.CACHE_BACKEND
-            settings.CACHE_BACKEND += '-test'
-            print "Switching to test cache: %s" % settings.CACHE_BACKEND
-
-    def restore_cache(self):
-        if self.stored_cache_backend:
-            cache_backend = settings.CACHE_BACKEND
-            # if the cache was used, remove directory & all cache files
-            cache_dir = cache_backend[len('file://'):]
-            if os.path.exists(cache_dir):
-                rmtree(cache_dir)
-            settings.CACHE_BACKEND = self.stored_cache_backend
-            print "Restoring cache: %s" % settings.CACHE_BACKEND
-
-alternate_test_cache = CacheTestWrapper
-
-class KeepTextTestRunner(unittest.TextTestRunner):
-    def run(self, test):
-        def wrapped_test(result):
-            with nested(fedora_testutil.alternate_test_fedora(),
-                    existdb_testutil.alternate_test_existdb(),
-                    alternate_test_cache()):
-                return test(result)
-        return super(KeepTextTestRunner, self).run(wrapped_test)
-
-
-class KeepTextTestSuiteRunner(DjangoTestSuiteRunner):
-    def run_suite(self, suite, **kwargs):
-        return KeepTextTestRunner(verbosity=self.verbosity,
-                                  failfast=self.failfast).run(suite)
-
-
-try:
-    # when xmlrunner is available, define xml test variants too
-
-    import xmlrunner
-
-    class KeepXmlTestRunner(xmlrunner.XMLTestRunner):
-        def __init__(self):
-            verbose = getattr(settings, 'TEST_OUTPUT_VERBOSE', False)
-            descriptions = getattr(settings, 'TEST_OUTPUT_DESCRIPTIONS', False)
-            output = getattr(settings, 'TEST_OUTPUT_DIR', 'test-results')
-
-            super_init = super(KeepXmlTestRunner, self).__init__
-            #super_init(verbose=verbose, descriptions=descriptions, output=output)
-            super_init(descriptions=descriptions, output=output)
-
-        def run(self, test):
-            def wrapped_test(result):
-                with nested(fedora_testutil.alternate_test_fedora(),
-                        existdb_testutil.alternate_test_existdb(),
-                        alternate_test_cache()):
-                    return test(result)
-            return super(KeepXmlTestRunner, self).run(wrapped_test)
-
-    class KeepXmlTestSuiteRunner(KeepTextTestSuiteRunner):
-        def run_suite(self, suite, **kwargs):
-            return KeepXmlTestRunner().run(suite)
-
-
-except ImportError:
-    pass
