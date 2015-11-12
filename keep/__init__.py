cca07794a8061064a3aba79f72bc4ec14da913d5
rlskoeser <rebecca.s.koeser@emory.edu>
2015-11-12 10:29:45 -0500

    Bump develop version to 2.5 after tagging 2.4

--- keep/__init__.py
+++ keep/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-__version_info__ = (2, 4, 1, None)
+__version_info__ = (2, 5, 0, 'dev')
 
 
 # Dot-connect all but the last. Last is dash-connected if not None.
