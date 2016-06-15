aa23668e9ea9de64464b5dd02f374550bb381a9c
Alex Zotov <alexandr.zotov@emory.edu>
2016-06-15 12:34:26 -0400

    chenge fabfile to adjust RHEL server

--- fabfile.py
+++ fabfile.py
@@ -186,7 +186,7 @@ def check_sysdeps():
     with settings(hide('stdout'),       # suppress full dpkg output
                   warn_only=True):      # don't abort on dpkg not-installed error code
         for pkg in env.system_packages:
-            if 'ii' not in run('dpkg -l %s' % pkg):
+            if 'ImageMagick' not in run('yum list installed | grep %s' % pkg):
                 not_installed.append(pkg)
 
     if not_installed:
