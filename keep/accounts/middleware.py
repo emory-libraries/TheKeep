e6ed69777e559d9aac112182b970db34998fd256
Alex Zotov <alexandr.zotov@emory.edu>
2016-07-08 14:19:55 -0400

    fixing print outs

--- keep/accounts/middleware.py
+++ keep/accounts/middleware.py
@@ -5,7 +5,6 @@ class ResearcherAccessMiddleware(object):
 
     def process_request(self, request):
         ip_addr = request.META.get('REMOTE_ADDR', None)
-        print ip_addr
         if request.user.is_anonymous() and ip_addr is not None and \
             ResearcherIP.objects.filter(ip_address__exact=ip_addr).count():
             request.user = AnonymousResearcher()
