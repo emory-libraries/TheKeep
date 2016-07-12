daa0c6046bbbe3952f2b8a203db9d177980b8af0
rlskoeser <rebecca.s.koeser@emory.edu>
2016-07-12 16:41:44 -0400

    Bump version to 2.6.4

--- keep/__init__.py
+++ keep/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 
-__version_info__ = (2, 6, 3, None)
-
+__version_info__ = (2, 6, 4, None)
 
 
 # Dot-connect all but the last. Last is dash-connected if not None.
