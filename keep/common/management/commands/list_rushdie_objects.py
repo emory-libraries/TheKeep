b41aef63d4e3685ca255f630ffb1be274c6a6fc6
Yang <yang@truman.edu>
2016-09-26 13:16:24 -0400

    swap out Repository class for ManagementRepository

--- keep/common/management/commands/list_rushdie_objects.py
+++ keep/common/management/commands/list_rushdie_objects.py
@@ -14,6 +14,30 @@ from keep.collection.models import CollectionObject
 from pidservices.djangowrapper.shortcuts import DjangoPidmanRestClient
 from progressbar import ProgressBar, Bar, Percentage, ETA, Counter
 from eulfedora.api import ResourceIndex
+from eulfedora import server
+
+class ManagementRepository(server.Repository):
+    '''Convenience class to initialize an instance of :class:`eulfedora.server.Repository`
+    with Fedora management/maintenance account credentials defined in Django settings.
+    .. Note::
+        This :class:`~eulfedora.server.Repository` variant should *only*
+        be used for maintainance tasks (e.g., scripts that ingest,
+        modify, or otherwise manage content).  It should **not** be
+        used for general website views or access; those views should
+        use the standard :class:`~eulfedora.server.Repository` which
+        will pick up the default, non-privileged credentials intended
+        for read and display access but not for modifying content in
+        the repository.
+    '''
+    default_pidspace = getattr(settings, 'FEDORA_PIDSPACE', None)
+    # default pidspace is not automatically pulled from django conf
+    # when user/password are specified, so explicitly set it here
+
+    def __init__(self):
+        # explicitly disabling other init args, so that anyone who tries to use
+        # this as a regular repo will get errors rather than confusing behavior
+        super(ManagementRepository, self).__init__(username=settings.FEDORA_MANAGEMENT_USER,
+                                                   password=settings.FEDORA_MANAGEMENT_PASSWORD)
 
 class Command(BaseCommand):
     """Manage command for listing Rushdie objects
@@ -38,7 +62,7 @@ class Command(BaseCommand):
         self.pidman = self.get_pidman()
 
         # initialize a Fedora repository object
-        self.repo = Repository()
+        self.repo = ManagementRepository()
 
         # send greeting message with instructions
         self.stdout.write(self.greetings)
@@ -101,7 +125,7 @@ class Command(BaseCommand):
             maxval=results_count).start()
 
         max_results_per_page = results["max_results_per_page"]
-        pages = int(math.ceil(results_count / max_results_per_page))
+        pages = int(math.ceil(results_count / float(max_results_per_page))) + 1
         current_count = 0
 
         # iterate through all results fetched from pidman
