7765c47201acb0dfa574cb0485637fc2b21c0044
rlskoeser <rebecca.s.koeser@emory.edu>
2016-09-27 11:11:02 -0400

    Move managment repository into keep.common.fedora for reuse

--- keep/common/fedora.py
+++ keep/common/fedora.py
@@ -447,6 +447,30 @@ class Repository(server.Repository):
         super(Repository, self).__init__(username=username, password=password)
 
 
+class ManagementRepository(server.Repository):
+    '''Convenience class to initialize an instance of :class:`eulfedora.server.Repository`
+    with Fedora management/maintenance account credentials defined in Django settings.
+    .. Note::
+        This :class:`~eulfedora.server.Repository` variant should *only*
+        be used for maintainance tasks (e.g., scripts that ingest,
+        modify, or otherwise manage content).  It should **not** be
+        used for general website views or access; those views should
+        use the standard :class:`~eulfedora.server.Repository` which
+        will pick up the default, non-privileged credentials intended
+        for read and display access but not for modifying content in
+        the repository.
+    '''
+    default_pidspace = getattr(settings, 'FEDORA_PIDSPACE', None)
+    # default pidspace is not automatically pulled from django conf
+    # when user/password are specified, so explicitly set it here
+
+    def __init__(self):
+        # explicitly disabling other init args, so that anyone who tries to use
+        # this as a regular repo will get errors rather than confusing behavior
+        super(ManagementRepository, self).__init__(username=settings.FEDORA_MANAGEMENT_USER,
+                                                   password=settings.FEDORA_MANAGEMENT_PASSWORD)
+
+
 class TypeInferringRepository(server.TypeInferringRepository, Repository):
     '''Make :class:`eulfedora.server.TypeInferringRepository`
     available with local defaults, based on :class:`Repository`
--- keep/common/management/commands/list_rushdie_objects.py
+++ keep/common/management/commands/list_rushdie_objects.py
@@ -4,7 +4,7 @@ from io import BytesIO
 from optparse import make_option
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from keep.common.fedora import Repository
+from keep.common.fedora import ManagementRepository
 from keep.arrangement.models import ArrangementObject
 from keep.audio.models import AudioObject
 from keep.file.models import DiskImage
@@ -16,28 +16,6 @@ from progressbar import ProgressBar, Bar, Percentage, ETA, Counter
 from eulfedora.api import ResourceIndex
 from eulfedora import server
 
-class ManagementRepository(server.Repository):
-    '''Convenience class to initialize an instance of :class:`eulfedora.server.Repository`
-    with Fedora management/maintenance account credentials defined in Django settings.
-    .. Note::
-        This :class:`~eulfedora.server.Repository` variant should *only*
-        be used for maintainance tasks (e.g., scripts that ingest,
-        modify, or otherwise manage content).  It should **not** be
-        used for general website views or access; those views should
-        use the standard :class:`~eulfedora.server.Repository` which
-        will pick up the default, non-privileged credentials intended
-        for read and display access but not for modifying content in
-        the repository.
-    '''
-    default_pidspace = getattr(settings, 'FEDORA_PIDSPACE', None)
-    # default pidspace is not automatically pulled from django conf
-    # when user/password are specified, so explicitly set it here
-
-    def __init__(self):
-        # explicitly disabling other init args, so that anyone who tries to use
-        # this as a regular repo will get errors rather than confusing behavior
-        super(ManagementRepository, self).__init__(username=settings.FEDORA_MANAGEMENT_USER,
-                                                   password=settings.FEDORA_MANAGEMENT_PASSWORD)
 
 class Command(BaseCommand):
     """Manage command for listing Rushdie objects
